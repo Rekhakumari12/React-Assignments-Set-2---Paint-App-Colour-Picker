37dafb69ef1edefc36f5414695156c1b7da55593
Rekha-1 <maneeta808@gmail.com>
2021-05-13 10:13:42 +0530

    commit1

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,10 @@
-
+import React from 'react';
+const Selection = (params) => {
+  const { key, applyColr } = params;
+  return (
+    <div className="fix-box" style={{background:applyColr}}>
+      <h2 className="subheading">Selection</h2>
+    </div>
+  )
+}
+export default Selection;
