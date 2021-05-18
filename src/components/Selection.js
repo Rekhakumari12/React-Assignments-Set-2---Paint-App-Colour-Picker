8f90c1638afea5d0d240aec50ded0dbb76b0ba60
Rekha-1 <maneeta808@gmail.com>
2021-05-18 15:50:45 +0530

    done

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../styles/Child.css';
 const Selection = (params) => {
-  const { key, applyColr } = params;
+  const {applyColor} = params;
+  const [styleColor, updateSelectionStyle] = useState({ background: '' })
+  console.log(styleColor);
   return (
-    <div className="fix-box" style={{background:applyColr}}>
+    <div className="fix-box" onClick={()=>applyColor(updateSelectionStyle)} style={{background:styleColor.background}}>
       <h2 className="subheading">Selection</h2>
     </div>
   )
